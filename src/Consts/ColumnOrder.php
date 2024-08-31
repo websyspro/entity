0ee54bb5f2857b02c26dace957b874b72844ca3e
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2024-08-31 19:09:44 -0300

    Update componentes

---
+++ src/Consts/ColumnOrder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Websyspro\Entity\Consts
+{
+  class ColumnOrder
+  {
+    public static array $Header = [
+      "Id"
+    ];
+
+    public static array $Body = [
+      "Id",
+      "Actived",
+      "ActivedBy",
+      "ActivedAt",
+      "CreatedBy",
+      "CreatedAt",
+      "UpdatedBy",
+      "UpdatedAt",
+      "Deleted",
+      "DeletedBy",
+      "DeletedAt"
+    ];
+
+    public static array $Footer = [
+      "Actived",
+      "ActivedBy",
+      "ActivedAt",
+      "CreatedBy",
+      "CreatedAt",
+      "UpdatedBy",
+      "UpdatedAt",
+      "Deleted",
+      "DeletedBy",
+      "DeletedAt"
+    ];
+  }
+}
