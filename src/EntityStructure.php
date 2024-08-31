e9ade672a5f5ddd545fa2a94ca4230647f8bc34f
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2024-08-31 17:28:11 -0300

    Atualizar componentes

--- src/EntityStructure.php
+++ src/EntityStructure.php
@@ -2,11 +2,23 @@
 
 namespace Websyspro\Entity
 {
+use Websyspro\Reflect\ClassReflectLoader;
   class EntityStructure
   {
     function __construct(
       public string $Entity,
       public array $Properties = []
-    ){}
+    ){
+      $this->SetEntityStructure();
+    }
+
+    function SetEntityStructure(
+    ): void {
+      $ClassReflectLoader = new ClassReflectLoader(
+        objectOrClass: $this->Entity
+      );
+
+      print_r($ClassReflectLoader);
+    }
   }
 }
