d7847aea14b5e1e107a7971f2da0ccbf61bee948
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2024-08-31 23:42:04 -0300

    Update componentes

--- src/EntityPersisted.php
+++ src/EntityPersisted.php
@@ -15,6 +15,7 @@ namespace Websyspro\Entity
     ){
       $this->SetEntityCreated();
       $this->SetEntityCreatedIndexes();
+      $this->SetEntityCreatedUniques();
     }
 
     private function HasEntityExists(
@@ -85,35 +86,61 @@ namespace Websyspro\Entity
     }
 
     private function SetCreatedIndexes(
-      string $entity,
-      array $structure = [],
-      array $structureEntity = []
+      string $Entity,
+      array $Structure = [],
+      array $StructureEntitys = []
     ): void {
-      if( $structure[EntityVersion::$New] instanceof EntityStructure ) {
-        $structureEntity = Utils::Mapper($structure[EntityVersion::$New]->ObterConstraintIndexes(), 
-          fn(array $propertys, string $key) => (
+      if( $Structure[EntityVersion::$New] instanceof EntityStructure ) {
+        $StructureEntitys = Utils::Mapper($Structure[EntityVersion::$New]->ObterConstraintIndexes(), 
+          fn(array $propertys) => (
             Utils::Mapper($propertys, fn(array $indexArr) => ( 
-              Utils::Join(array_merge(
-                [ "Idx", EntityUtils::ObterEntityName($entity) ], $indexArr
-              ), "_")
+              [ Utils::Join( array_merge(
+                [ "Idx", EntityUtils::ObterEntityName($Entity) ], $indexArr
+                ), "_"), Utils::Join( $indexArr, ", ")]
             ))
           )
         );
 
-        print_r($structureEntity);
+        Utils::Mapper($StructureEntitys, fn(array $StructureEntity ) => (
+          Utils::Mapper($StructureEntity, fn($IndexesList) => (
+            $this->persistedArr[] = call_user_func_array("sprintf", array_merge(
+              [ "alter table `%s` add index `%s` (%s)", EntityUtils::ObterEntityName($Entity) ], $IndexesList
+            ))
+          ))
+        ));
       }
     }
 
     private function SetEntityCreatedIndexes(
     ): void {
       Utils::Mapper( $this->Items, 
-        fn(array $structure, string $entity) => (
+        fn(array $Structure, string $Entity) => (
           $this->SetCreatedIndexes(
-            structure: $structure,
-            entity: $entity
+            Structure: $Structure,
+            Entity: $Entity
           )
         )
       );
+    }
+
+    private function SetCreatedUniques(
+      string $Entity,
+      array $Structure = [],
+      array $StructureEntitys = []
+    ): void {
+      
+    }    
+
+    private function SetEntityCreatedUniques(
+    ): void {
+      Utils::Mapper( $this->Items, 
+      fn(array $Structure, string $Entity) => (
+        $this->SetCreatedUniques(
+          Structure: $Structure,
+          Entity: $Entity
+        )
+      )
+    );;
     }
   }
 }
