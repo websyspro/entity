a45bc6abe7924590bdf92539a19284d29f1dadaf
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2025-06-11 17:11:51 -0300

    Update componentes

--- src/Enums/ColumnType.php
+++ src/Enums/ColumnType.php
@@ -116,6 +116,8 @@ enum ColumnType: string
     }
 
     return match( $this ){
+      ColumnType::Date => $this->dateDecode($mixed),
+      ColumnType::Datetime => $this->datetimeDecode($mixed),
       ColumnType::Decimal => $this->decimalDecode($mixed),
       ColumnType::Flag => $this->flagDecode($mixed),
         default => $mixed
