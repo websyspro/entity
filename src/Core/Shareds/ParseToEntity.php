6c28c08f94466d6e3106c66a08e71c47c4508c0d
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2025-06-17 10:07:24 -0300

    Update componentes

---
+++ src/Core/Shareds/ParseToEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Websyspro\Entity\Core\Shareds;
+
+class StdClassToEntity
+{
+  public static function ParseEntity(
+    array|object $stdClass,
+    string $entity
+  ): object {
+    $newEntity = new $entity;
+
+    foreach($stdClass as $key => $val){
+      $newEntity->{$key} = $val;
+    }
+    
+    return $newEntity;
+  }
+}
