c17f864170283bc7cb241b7317e1fdcaa0b4cec0
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-02-21 13:06:09 -0300

    Update components

--- src/Shareds/Reference.php
+++ src/Shareds/Reference.php
@@ -6,11 +6,13 @@ class Reference
 {
   public string $table;
   public string $key;
+  public string $entity;
 
   public function __construct(
     EntityStructure $reference
   ){
     $this->table = $reference->entity->table;
+    $this->entity = $reference->entity->class;
     if( $reference->primaryKey->exist() === true ){
       $this->key = $reference->primaryKey->first()->name;
     }
