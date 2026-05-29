997f7f581a18dab46ace23b8cb57a7bc307a4740
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-05-29 19:09:42 -0300

    Update components

---
+++ src/Enums/MetaType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Websyspro\Entity\Enums;
+
+enum MetaType:string
+{
+  case Query = "query";
+  case Schema = "schema";
+}
--- test/main.php
+++ test/main.php
@@ -1,7 +1,9 @@
 <?php
 
 use Websyspro\Entity\Shareds\TokensByClosure;
-use Websyspro\Test\Crm\Entitys\PropostaEntity;
+use Websyspro\Test\Crm\Entitys\ItemPropostaEntity;
+use Websyspro\Test\Crm\Entitys\PropostaEntity as Props;
+use Websyspro\Test\Enums\Status;
 
 $start = microtime(true);
 
@@ -16,16 +18,25 @@ class UserRepository
 
   public function getAll(
   ): array {
-    $tokensByClosure = new TokensByClosure(
-      fn( PropostaEntity $i ) => (
-        $i->IsActive == true &&
-        $i->IsDeleted == false &&
-        $i->Id == [
+    $concate = "Test";
+    $escolaA = "Emerson";
+
+    $tokensByClosure = new TokensByClosure(fn( Props $p ) => (
+        $p->NomeContato->trim() == 'TEST' &&
+        !$p->IsActive &&
+        'TEST' == $p->NomeContato &&
+        $p->NomeContato->trim()->startWith( "Test" ) &&
+        $p->IsDeleted == false &&
+        $p->Id == [
           '0303AE33-D883-43C5-262B-08DBD9497C02',
           '0303AE33-D883-43C5-262B-08DBD9497C02',
           '0303AE33-D883-43C5-262B-08DBD9497C02',
           '0303AE33-D883-43C5-262B-08DBD9497C02'
-        ]
+        ] &&
+        $p->NomeProposta === [ 'Item 1 ' . $concate, "{$escolaA}", Status::Aprovada->value, Status::RevisaoGerentePendente, 12 ] &&
+        !$p->itemsProposta->any( fn(ItemPropostaEntity $i ) => (
+          $i->IsActive == true && $i->IsDeleted == false && $i->PropostaId == $p->Id
+        )) && !$p->IsActive
       )
     );
 
@@ -37,5 +48,5 @@ $UserRepository = new UserRepository();
 $getAll = $UserRepository->getAll();
 
 $end = ( microtime( true ) - $start ) * 1000;
-echo "Timer: {$end}\n";
+echo "Timer: {$end}(ms)\n";
 print_r($getAll);
