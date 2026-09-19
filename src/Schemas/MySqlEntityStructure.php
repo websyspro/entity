21f4b91c3f4ec235aa42441802d74df891c434d2
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-19 19:38:26 -0300

    Release 1.0.19

--- src/Schemas/MySqlEntityStructure.php
+++ src/Schemas/MySqlEntityStructure.php
@@ -4,9 +4,13 @@ namespace Websyspro\Entity\Schemas;
 
 use Websyspro\Entity\Decorators\Column;
 use Websyspro\Entity\Decorators\ForeignKey;
+use Websyspro\Entity\Decorators\Index;
 use Websyspro\Entity\Decorators\InitialDefault;
 use Websyspro\Entity\Decorators\Length;
+use Websyspro\Entity\Decorators\Precision;
+use Websyspro\Entity\Decorators\PrimaryKey;
 use Websyspro\Entity\Decorators\Required;
+use Websyspro\Entity\Decorators\Unique;
 use Websyspro\Entity\Interfaces\ForeignKeyStructure;
 use Websyspro\Entity\Interfaces\PrecisionDetails;
 use Websyspro\Entity\Types\ColumnAutoIncrement;
@@ -60,7 +64,7 @@ extends AbstractEntityStructure
     string $name,
     mixed $instance
   ): void {
-    if( $instance instanceof PrecisionDetails ){
+    if( $instance instanceof Precision ){
       $this->precisions->items[ $name ] = new PrecisionDetails(
         $instance->precision, $instance->scale
       );
